d9fc05d23e2d446cb23f364af403d24e8adf7894
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:30:15 -0300

    Update GrammarSymbols.java

--- src/parser/GrammarSymbols.java
+++ src/parser/GrammarSymbols.java
@@ -3,10 +3,12 @@ package parser;
 /**
  * This class contains codes for each grammar terminal
  * 
- * @version 2010-september-04
- * @discipline Compiladores
+ * @version 2016-march-22
+ * @course Compiladores
  * @author Gustavo H P Carvalho
  * @email gustavohpcarvalho@ecomp.poli.br
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
  */
 public class GrammarSymbols {
 
