625be28966200b352497412470140e7352820637
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:31:34 -0300

    Update TestParser.java

--- src/parser/TestParser.java
+++ src/parser/TestParser.java
@@ -1,5 +1,14 @@
 package parser;
 
+/**
+ * TestParser class
+ * 
+ * @version 2016-march-22
+ * @course Compiladores
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
+ */
+ 
 public class TestParser {
 	public static void main(String[] args) {
 		Parser test = new Parser();
