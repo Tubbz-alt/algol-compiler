7d93759ccb36687a38b76adf5f11b595a98ca1f3
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:21:57 -0300

    Update TestScanner.java

--- src/scanner/TestScanner.java
+++ src/scanner/TestScanner.java
@@ -2,6 +2,15 @@ package scanner;
 
 import parser.GrammarSymbols;
 
+/**
+ * TestScanner class
+ * 
+ * @version 2016-march-22
+ * @course Compiladores
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
+ */
+ 
 public class TestScanner {
 	
 	public static void main(String[] args) {
