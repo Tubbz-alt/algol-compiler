4872bd17f2ff39252b49840c9ab555a547297c11
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:28:23 -0300

    Update Instruction.java

--- src/encoder/Instruction.java
+++ src/encoder/Instruction.java
@@ -1,5 +1,13 @@
 package encoder;
 
+/**
+ * Instruction class
+ * 
+ * @version 2016-march-22
+ * @course Compiladores
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
+ */
 public class Instruction {
 	private String instruction;
 	
