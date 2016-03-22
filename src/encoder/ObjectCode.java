9a4ecd3cab2a203baa8acd4f6fbc0b07d1a2c97c
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:28:57 -0300

    Update ObjectCode.java

--- src/encoder/ObjectCode.java
+++ src/encoder/ObjectCode.java
@@ -1,5 +1,13 @@
 package encoder;
 
+/**
+ * ObjectCode class
+ * 
+ * @version 2016-march-22
+ * @course Compiladores
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
+ */
 public class ObjectCode {
 	private StringBuffer extern, data, text;
 	
