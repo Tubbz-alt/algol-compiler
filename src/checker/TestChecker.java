d6c2275a5d87ba3a4d8dd2b5f611162242bd7d55
Pedro Queiroz <pedro.queiroz@liferay.com>
2016-03-22 17:25:50 -0300

    Update TestChecker.java

--- src/checker/TestChecker.java
+++ src/checker/TestChecker.java
@@ -2,6 +2,15 @@ package checker;
 
 import parser.Parser;
 
+/**
+ * TestChecker class
+ * 
+ * @version 2016-march-22
+ * @course Compiladores
+ * @author Pedro H Q Santos
+ * @email phqs@ecomp.poli.br
+ */
+
 public class TestChecker {
 	public static void main(String[] args) {
 		Parser parser = new Parser();
