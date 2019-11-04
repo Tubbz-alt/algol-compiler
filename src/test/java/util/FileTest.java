49dafbe8ea09c00bf1a6afdabb9ec940a1eb1d99
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-11-04 00:55:22 -0300

    Refactor File tests

--- src/test/java/util/FileTest.java
+++ src/test/java/util/FileTest.java
@@ -1,25 +1,35 @@
 package util;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileTest {
 
-    private File file;
-
-    @BeforeEach
-    void setUp() {
-        file = new File("src/test/resources/samples/sample1.alg");
-    }
-
     @Test
     void shouldReadSingleCharFromFile() {
+        File file = new File("src/test/resources/util/samples/sample1.alg");
+
         assertEquals('b', file.readChar());
         assertEquals('e', file.readChar());
         assertEquals('g', file.readChar());
         assertEquals('i', file.readChar());
         assertEquals('n', file.readChar());
+
+        assertEquals(' ', file.readChar());
+
+        assertEquals('t', file.readChar());
+        assertEquals('o', file.readChar());
+
+        assertEquals('\n', file.readChar());
+        assertEquals('\n', file.readChar());
+
+        assertEquals('e', file.readChar());
+        assertEquals('n', file.readChar());
+        assertEquals('d', file.readChar());
+
+        assertEquals('\n', file.readChar());
+
+        assertEquals('\0', file.readChar());
     }
 }
